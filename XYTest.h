5e3e5bce74c10738e7b5ec23bacfced65c6ffb41
Xiaoyan Yan <XiaoyanYan@Xiaoyans-MacBook-Air.local>
2014-09-14 07:58:56 -0500

    added a test class

---
+++ XYTest.h
@@ -0,0 +1,13 @@
+//
+//  XYTest.h
+//  Git Test
+//
+//  Created by Xiaoyan Yan on 9/14/14.
+//  Copyright (c) 2014 Xiaoyan Yan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XYTest : NSObject
+
+@end
---
+++ XYTest.m
@@ -0,0 +1,13 @@
+//
+//  XYTest.m
+//  Git Test
+//
+//  Created by Xiaoyan Yan on 9/14/14.
+//  Copyright (c) 2014 Xiaoyan Yan. All rights reserved.
+//
+
+#import "XYTest.h"
+
+@implementation XYTest
+
+@end
